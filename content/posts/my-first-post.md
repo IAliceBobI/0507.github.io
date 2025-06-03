6b99f939da6faf175a1b380ff2483b56ff160642
008 <007>
2025-06-03 10:49:11 +0800

    learn

--- content/posts/my-first-post.md
+++ content/posts/my-first-post.md
@@ -1,6 +1,6 @@
 +++
 date = '2025-06-03T01:04:05+08:00'
-draft = false
+draft = true
 title = '开始'
 +++
 
---
+++ run.ps1
@@ -0,0 +1,6 @@
+hugo server --navigateToChanged --buildDrafts 
+
+# hugo --buildDrafts    # or -D
+# hugo --buildExpired   # or -E
+# hugo --buildFuture    # or -F
+
